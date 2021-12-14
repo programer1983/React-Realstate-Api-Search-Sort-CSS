bed9cb79337a3adc4224aebf9dd243a6d6384df3
programer1983 <programer972@gmail.com>
2021-12-14 13:17:16 +0200

    creation of data search components by categories - rent, property;

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,17 @@
+import {useEffect, useState} from 'react'
+import {Flex, Select, Box, Text, Input, Spinner, Icon, Button} from "@chakra-ui/react"
+import {useRouter} from "next/router"
+import {MyCancel} from "react-icons/md"
+import Inmage from "next/image"
+
+
+
+const SearchFilters = () => {
+    return (
+        <div>
+            SearchFilters
+        </div>
+    )
+}
+
+export default SearchFilters
