cef24101c2415f88bcfc330b4cd2b57b64fcb12f
programer1983 <programer972@gmail.com>
2021-12-16 21:37:27 +0200

    creating a search filter;

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,15 +3,44 @@ import {Flex, Select, Box, Text, Input, Spinner, Icon, Button} from "@chakra-ui/
 import {useRouter} from "next/router"
 import {MyCancel} from "react-icons/md"
 import Inmage from "next/image"
+import {filterData, getFilterValues} from "./../utils/filterData"
+import router from "next/router"
 
 
 
 const SearchFilters = () => {
-    const [filters, setFilters] = useState({})
+    const [filters, setFilters] = useState(filterData)
+
+    const searchProperties = (filterValues) => {
+        const path = router.pathname
+        const {query} = router
+
+        const values = getFilterValues(filterValues)
+
+        values.forEach((item) => {
+            query[item.name] = item.value
+        })
+        router.push({pathname: path, query})
+    }
     
     return (
         <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="warp">
-
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                  <Select
+                    placeholder={filter.placeholder}
+                    w="fit-content"
+                    p="2"
+                    onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}
+                    >
+                        {filter?.items?.map((item) => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
+                  </Select>
+                </Box>
+            ))}
         </Flex>
     )
 }
