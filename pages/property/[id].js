56b3c7a19263ecfe3e45ac89e6ad552d598569ff
programer1983 <programer972@gmail.com>
2021-12-19 12:01:13 +0200

    creating a scrolling bar for photos, fixing errors;

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -3,19 +3,20 @@ import {FaBed, FaBath} from "react-icons/fa"
 import {BsGridFill} from "react-icons/bs"
 import {GoVerified} from "react-icons/go"
 import millify from "millify"
-import {fetchApi, baseUrl} from "./../property/[id"
+import {fetchApi, baseUrl} from "./../../utils/fetchApi"
+import ImageScrollbar from "./../../components/ImageScrollbar"
 
 
-const PropertyDeteil = ({propertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos, }}) => (
+const PropertyDeteils = ({propertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos, }}) => (
     <Box maxWidth="1000px" margin="auto" p="4">
         {photos && <ImageScrollbar data={photos}/>}
     </Box>
 )
 
-export default PropertyDeteil
+export default PropertyDeteils
 
-export async function getServerSideProps ({params: {id}}) {
-    const data = fetchApi(`${baseUrl}/properties/detail?externalID=${id}`)
+export async function getServerSideProps({params: {id}}) {
+    const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`)
     return {
         props: {
             propertyDetails: data
